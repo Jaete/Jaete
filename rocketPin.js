c94465c1feb0cf72f1fc718a64388dc2e905f7bc
Victor Jaete <111895259+Jaete@users.noreply.github.com>
2023-01-04 09:09:08 -0300

    Update rocketPin.js

--- rocketPin.js
+++ rocketPin.js
@@ -1,4 +1,7 @@
 function typeNumber(typedNumber) {
+    if(query.value("pinRocket") === ""){
+        document.getElementById("pinRocket").style.letterSpacing = "4.4em";
+    }
     if(query.value("pinRocket").length < 4){
         setElement("pinRocket", query.value("pinRocket") + typedNumber.toString());
     }
