466b5004b889c40f9b95c300a1cfd14b93c8d768
Victor Jaete <111895259+Jaete@users.noreply.github.com>
2023-01-04 09:00:59 -0300

    Update rocketPin.js

--- rocketPin.js
+++ rocketPin.js
@@ -1,3 +1,31 @@
-function verifySuccess(typedNumber) {
-    document.getElementById("pinRocket").value = toString(typedNumber);
+function typeNumber(typedNumber) {
+    if(query.value("pinRocket").length < 4){
+        setElement("pinRocket", query.value("pinRocket") + typedNumber.toString());
+    }
+    verifySucess();
+}
+
+function verifySucess(){
+    if(query.value("pinRocket") === "9647"){
+        document.getElementById("successNotifyRocket").style.display = "inline";
+    }
+}
+
+const query = {
+    value: (selectorName) => {
+        let query = document.querySelector("#" + selectorName).value;
+        return query;     
+    },
+    check: (selectorName) => {
+        let query = document.querySelector("#" + selectorName).checked;
+        return query;
+    },
+    radio: (selectorName) =>{
+        let query = document.querySelector(`input[name="`+selectorName+`"]:checked`).value;
+        return query;
+    }
+}
+
+const setElement = (selectorName, value) => {
+    document.getElementById(selectorName).value = value;
 }
